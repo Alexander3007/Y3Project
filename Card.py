818c2f69860161bb443ac01caffdac67bd644cf7
MinasMayth <minasmayth@protonmail.com>
2022-04-14 13:27:14 +0200

    Can cycle through the cards now

--- Card.py
+++ Card.py
@@ -1,29 +1,25 @@
 import pygame
 
 # Global Variables
-
-
-width = 200
-height = 250
-
-white = (255,255,255)
+white = (255, 255, 255)
 
 
 class Card(pygame.sprite.Sprite):
-    def __init__(self, width, height, ID ):
+    def __init__(self, display_width, display_height, ID):
         super().__init__()
-        self.width = width
-        self.height = height
 
         # Set dimensions for the card
-        self.surface = pygame.Surface([width, height])
+        self.surface = pygame.Surface([display_width, display_height])
         self.ID = ID
 
         # Load the image
-        self.image = pygame.image.load(r'images/Card' + str(self.ID) + '.png')
+        self.image = pygame.image.load(r'images/Cards/Card' + str(self.ID) + '.png')
 
         self.rect = self.image.get_rect()
-        self.rect.center = (600,400)
-    def display_image(self):
-        self.surface.fill(white)
-        self.surface.blit(self.image,(0,0))
+        self.rect.center = (display_width/2, display_height/2)
+
+    def support_action(self):
+        print("Hello")
+
+    def denounce_action(self):
+        print("hello there")
--- Menu.py
+++ Menu.py
@@ -4,61 +4,35 @@ from pygame_menu import sound
 import urllib.request
 from Card import Card
 
-pygame.init()
-# assigning values to X and Y variable
-X = 1200
-Y = 800
-surface = pygame.display.set_mode((X, Y))
-
-#Handles the sounds
-
-#engine = sound.Sound()
-#engine.set_sound(sound.SOUND_TYPE_CLICK_MOUSE, '/home/me/click.ogg')
-#engine.set_sound(sound.SOUND_TYPE_OPEN_MENU, '/home/me/open.ogg')
 
 def start_the_game():
+    card_iterator = 0
     # define the RGB value for white,
     #  green, blue colour .
     white = (255, 255, 255)
     green = (0, 255, 0)
     blue = (0, 0, 128)
 
-
-    # Create a for loop to instantiate cards
+    smallfont = pygame.font.SysFont('Corbel', 32)
 
     # create the display surface object
     # of specific dimension..e(X, Y).
     display_surface = pygame.display.set_mode((X, Y))
 
     # set the pygame window name
-    pygame.display.set_caption('Show Text')
+    pygame.display.set_caption('The Game')
 
-    # create a font object.
-    # 1st parameter is the font file
-    # which is present in pygame.
-    # 2nd parameter is size of the font
-    font = pygame.font.Font('freesansbold.ttf', 24)
+    #Set the cards up!
+    all_cards = []
 
-    # create a text surface object,
-    # on which text is drawn on it.
-    text = font.render('The game will appear here...', True, green, blue)
+    card_number = 13
+    for i in range(0, card_number-1):
+        all_cards.append(Card(X, Y, i))
 
-    # create a rectangular object for the
-    # text surface object
-    textRect = text.get_rect()
-
-    # set the center of the rectangular object.
-    textRect.center = (X // 2, Y // 2)
-
-
-    allCards = pygame.sprite.Group()
-
-    card = Card(200, 300, 0)
-
-    allCards.add(card)
-
-    # infinite loop
+    # game loop
     while True:
+        card = all_cards[card_iterator]
+        mouse = pygame.mouse.get_pos()
 
         # completely fill the surface object
         # with white color
@@ -69,9 +43,33 @@ def start_the_game():
         # at the center coordinate.
         display_surface.blit(card.image, card.rect)
 
+        # Button Displayable
+        # Support button
+        pygame.draw.rect(display_surface, color_dark, [(X / 2) + (X / 4) - 200, Y - 50, 175, 40])
+
+        text = smallfont.render('SUPPORT', True, color_light)
+        display_surface.blit(text, ((X / 2) + (X / 4) - 175 - 1, Y - 50 + 5))
+
+        # Denounce Button
+        pygame.draw.rect(display_surface, color_dark, [(X / 2) - (X / 4), Y - 50, 175, 40])
+
+        text = smallfont.render('DENOUNCE', True, color_light)
+        display_surface.blit(text, ((X / 2) - (X / 4) + 10, Y - 50 + 5))
+
         # iterate over the list of Event objects
         # that was returned by pygame.event.get() method.
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Denounce Action
+                if (X / 2) - (X / 4) <= mouse[0] <= (X / 2) - (X / 4) + 175 and Y - 50 <= mouse[1] <= Y - 50 + 40:
+                    print("wolololo!")
+                    card_iterator += 1
+
+                # Support Action
+                if (X / 2) + (X / 4) - 200 <= mouse[0] <= (X / 2) + (X / 4) - 200 + 175 and Y - 50 <= mouse[
+                    1] <= Y - 50 + 40:
+                    print("wolololo?")
+                    card_iterator += 1
 
             # if event object type is QUIT
             # then quitting the pygame
@@ -79,33 +77,49 @@ def start_the_game():
             if event.type == pygame.QUIT:
                 # deactivates the pygame library
                 pygame.quit()
-
                 # quit the program.
                 quit()
 
             # Draws the surface object to the screen.
-            
+
             card.update()
-            card.display_image()
 
             pygame.display.update()
-    pass
+
+
+            # Check if card maximum has been reached
+            if card_iterator > card_number - 2:
+                menu.mainloop(display_surface)
+
 
 def learn_more():
     #For Later
     pass
 
 
+if __name__ == '__main__':
+    pygame.init()
+    # assigning values to X and Y variable
+    X = 1200
+    Y = 700
+    surface = pygame.display.set_mode((X, Y))
 
+    color_dark = (100, 100, 100)
+    color_light = (170, 170, 170)
 
-menu = pygame_menu.Menu('Twitter', X, Y,
-                       theme=pygame_menu.themes.THEME_BLUE)
+    # Handles the sounds
 
-menu.add.text_input('Name:', default='John Doe')
+    # engine = sound.Sound()
+    # engine.set_sound(sound.SOUND_TYPE_CLICK_MOUSE, '/home/me/click.ogg')
+    # engine.set_sound(sound.SOUND_TYPE_OPEN_MENU, '/home/me/open.ogg')
 
+    menu = pygame_menu.Menu('Tweeter', X, Y,
+                            theme=pygame_menu.themes.THEME_BLUE)
 
-menu.add.button('Play', start_the_game)
-menu.add.button('Learn more',learn_more)
-menu.add.button('Quit', pygame_menu.events.EXIT)
+    menu.add.text_input('Name:', default='John Doe')
 
-menu.mainloop(surface)
+    menu.add.button('Play', start_the_game)
+    menu.add.button('Learn more', learn_more)
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+
+    menu.mainloop(surface)

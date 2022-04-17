c343079c0fb7c7f6d52621b9499e5dfdcc01439a
MinasMayth <minasmayth@protonmail.com>
2022-04-17 18:26:18 +0200

    Some documentation

--- Menu.py
+++ Menu.py
@@ -8,6 +8,10 @@ from Game import Game
 
 class Menu:
     def __init__(self):
+        """
+        Initialization function for the main menu. Here we set window size and add the different buttons
+        for the actions that the player can take.
+        """
         pygame.init()
         # assigning values to X and Y variable
         self.X = 1200
@@ -23,34 +27,31 @@ class Menu:
         # engine.set_sound(sound.SOUND_TYPE_CLICK_MOUSE, '/home/me/click.ogg')
         # engine.set_sound(sound.SOUND_TYPE_OPEN_MENU, '/home/me/open.ogg')
 
-        self.menu = pygame_menu.Menu('Tweeter', self.X, self.Y,
-                                theme=pygame_menu.themes.THEME_BLUE)
+        self.main_menu = pygame_menu.Menu('Tweeter', self.X, self.Y,
+                                          theme=pygame_menu.themes.THEME_BLUE)
 
-        self.menu.add.text_input('Name:', default='John Doe')
+        self.main_menu.add.text_input('Name:', default='John Doe')
 
-        self.menu.add.button('Play', self.start_the_game)
-        self.menu.add.button('Learn more!', self.learn_more)
-        self.menu.add.button('Quit', pygame_menu.events.EXIT)
+        self.main_menu.add.button('Play', self.start_the_game)
+        self.main_menu.add.button('Learn more!', self.learn_more)
+        self.main_menu.add.button('Quit', pygame_menu.events.EXIT)
 
-        self.menu.mainloop(self.surface)
-
+        self.main_menu.mainloop(self.surface)
 
     def start_the_game(self):
         """
-        Method to start a new game from the main menu and run the main game loop.
+        Method to start a new game from the main menu and run the game loop.
         """
-        game_instance = Game(self.X, self.Y, self.menu)
+        game_instance = Game(self.X, self.Y, self.main_menu)
         # game loop
         while True:
             game_instance.game_loop(self.X, self.Y)
 
-
-    def learn_more(self):
+    @staticmethod
+    def learn_more():
         """
         Method that will bring the user to a page where they can learn more about fake news.
         """
         request_url = urllib.request.urlopen(
             'https://docs.google.com/document/d/1BzQ7PH07unRiIMSIndmfJ2Iu9IIXOHgIvytI7JvgxHw/edit#heading=h.hwpaayxhxfw')
         print(request_url.read())
-
-
--- Utility.py
+++ Utility.py
@@ -3,10 +3,18 @@ import os
 
 
 def resource_path(relative_path):
+    """
+    Utility function for loading data (i.e. images) with a specific path in mind. Haven't fully understood
+    how to use it yet, as it will try and store data within the temp folder when running the game
+    as an executable after building with Pyinstaller. However, this data is then somehow not actually
+    read and causes the game to crash.
+    :param relative_path: relative path to data from the current file
+    :return: properly formatted path
+    """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
-    
+
     return os.path.join(base_path, relative_path)
